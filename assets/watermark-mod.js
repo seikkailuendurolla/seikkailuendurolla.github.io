ed77d2f821bbff06d45e5c3ddd68794e33e37b2e
Seikkailuendurolla <seikkailuendurol.la@gmail.com>
2018-04-02 14:41:32 +0300

    watermark sivun ensimmäinen prototyyppi

---
+++ assets/watermark-mod.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    var original;
+
+    document.getElementById("image").addEventListener("change", function (e) {
+        var preview = document.getElementById('preview')
+        watermark([e.target.files[0]])
+            .image(function(target) { return target;  })
+            .then(function (img) {
+                preview.appendChild(img);
+            })
+            .then(function () {
+                var img = preview.querySelector('img');
+                var original = img;
+                var watermarkImage = document.getElementById('watermark');
+                var file = watermarkImage.files[0];
+                watermark([original, file])
+                    .image(watermark.image["upperLeft"](0.5))
+                    .then(function(marked) {
+                        preview.replaceChild(marked, img);
+                });
+            });
+    });
+});
