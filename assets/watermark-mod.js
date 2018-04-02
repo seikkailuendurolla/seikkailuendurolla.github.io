246b45bf27892e94240946816a56492c2465eafa
Seikkailuendurolla <seikkailuendurol.la@gmail.com>
2018-04-02 14:59:46 +0300

    Refactorointia. Lisäksi mahdollisuus vaihtaa vesileiman sijaintia

--- assets/watermark-mod.js
+++ assets/watermark-mod.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     var original;
+    var preview = document.getElementById('preview')
 
     document.getElementById("image").addEventListener("change", function (e) {
-        var preview = document.getElementById('preview')
 
         watermark([e.target.files[0]])
             .image(function(target) { return target;  })
@@ -10,14 +10,32 @@ $(document).ready(function() {
                 preview.appendChild(img);
             })
             .then(function () {
-                var img = preview.querySelector('img');
-                var original = img;
-                var watermarkImage = document.getElementById('watermark').files[0];;
-                watermark([original, watermarkImage])
-                    .image(watermark.image["upperLeft"](0.5))
-                    .then(function(marked) {
-                        preview.replaceChild(marked, img);
-                });
+                updatePreview("upperLeft");
             });
     });
+
+    $('button').on("click", function(b) {
+        console.log("BUTTON");
+        var position = b.target.value;
+        console.log(position);
+
+        updatePreview(position);
+    });
+
+    function updatePreview(position) {
+        var img = preview.querySelector('img');
+
+        if (! original) {
+            original = img;
+        }
+
+        var watermarkImage = document.getElementById('watermark').files[0];
+
+        watermark([original, watermarkImage])
+            .image(watermark.image[position](0.5))
+            .then(function(marked) {
+                preview.replaceChild(marked, img);
+            });
+
+    }
 });
